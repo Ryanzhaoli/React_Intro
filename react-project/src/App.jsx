ff15f82c8833491fb9d68632c192d4a11c4933cb
Ryan Li <jijizaza999@gmail.com>
2023-10-05 11:27:16 -0700

    react-project done with UI

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,20 +1,16 @@
-import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
-import { HomePage, FilmsPage, SingleFilmPage } from "./pages/";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage, FilmsPage, SingleFilmPage } from "./pages";
 
-export default function App() {
-  return(
+function App() {
+  return (
     <BrowserRouter>
-      <nav>
-        <ul>
-          <li><NavLink to = "/">Home</NavLink></li>
-          <li><NavLink to = "films">Films</NavLink></li>
-        </ul>
-      </nav>
       <Routes>
-        <Route path ="/" element = {<HomePage/>}></Route>
-        <Route path = "/films" element = {<FilmsPage/>}></Route>
-        <Route path = "/films/film/:id" element = {<SingleFilmPage/>}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="films" element={<FilmsPage />} />
+        <Route path="films/:id" element={<SingleFilmPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
+
+export default App;
--- react-project/src/pages/films.page.jsx
+++ react-project/src/pages/films.page.jsx
@@ -1,86 +1,113 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { MainLayout, Loader } from "../components";
 import {
-  getListOf,
+  Form,
+  Card,
+  ListGroup,
+  ListGroupItem,
+  Row,
+  Col,
+  Badge,
+} from "react-bootstrap";
+import {
   filterFilmsByDirector,
   getFilmStats,
+  getListOf,
 } from "../helpers/film.helpers";
-import { Link } from "react-router-dom";
 
-export default function FilmsPage() {
+export function FilmsPage() {
   const [list, setList] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [searchDirector, setSearchDirector] = useState("");
 
-
-  const getFilms = () => {
+  useEffect(() => {
     fetch("https://studioghibliapi-d6fc8.web.app/films")
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((films) => {
-        console.log(films);
-        setList(films);
-        console.log(list);
+      .then((res) => res.json())
+      .then((data) => {
+        setList(data);
+        setIsLoaded(true);
       })
       .catch((err) => console.error(err));
-    console.log(list);
-  };
+  }, []);
 
-  useEffect(() => {
-    getFilms();
-  });
+  if (!isLoaded) {
+    return (
+      <MainLayout className="text-center">
+        <Loader size={40} />
+      </MainLayout>
+    );
+  }
 
-  const directors = getListOf(list, "director");
-
-  const filmsByDirector = filterFilmsByDirector(list, searchDirector);
-
-  const { avg_score, total, latest } = getFilmStats(filmsByDirector);
+  let directors = getListOf(list, "director");
+  let filmsByDirector = filterFilmsByDirector(list, searchDirector);
+  let { avg_score, latest, total } = getFilmStats(filmsByDirector);
 
   return (
-    <div>
-      <h1>Studio Ghibli Films</h1>
-      <form>
-        <div className="form-group">
-          <label></label>
-          <select
-            value={searchDirector}
-            onChange={(e) => setSearchDirector(e.target.value)}
-          >
-            <option value="">All</option>
-            {directors.map((director, index) => (
-              <option key={index} value={director}>
-                {director}
-              </option>
-            ))}
-          </select>
-        </div>
-      </form>
-      <div>
-        <div>
-          <span># Of Films:</span>
-          <span> {total}</span>
-        </div>
-        <div>
-          <span>Average Rating:</span>
-          <span> {avg_score.toFixed(2)}</span>
-        </div>
-        <div>
-          <span>Latest Film:</span>
-          <span> {latest}</span>
-        </div>
+    <MainLayout>
+      <div className="">
+        <h1>Studio Ghibli Films</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="searchDirector">
+            <Form.Label>Filter by Director</Form.Label>
+            <Form.Select
+              value={searchDirector}
+              onChange={(e) => setSearchDirector(e.target.value)}
+            >
+              <option value="">All</option>
+              {directors.map((item, idx) => {
+                return (
+                  <option key={idx} value={item}>
+                    {item}
+                  </option>
+                );
+              })}
+            </Form.Select>
+          </Form.Group>
+        </Form>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <div className="d-flex justify-content-between">
+                  <small># Of Films</small>
+                  <Badge bg="dark">{total}</Badge>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Body>
+                <div className="d-flex justify-content-between">
+                  <small>Average Rating</small>
+                  <Badge bg="dark">{avg_score.toFixed(2)}</Badge>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Body>
+                <div className="d-flex justify-content-between">
+                  <small>Latest Film</small>
+                  <Badge bg="dark">{latest}</Badge>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <hr />
+        <ListGroup>
+          {filmsByDirector.map((item) => {
+            return (
+              <ListGroupItem key={item.id}>
+                <Link to={`${item.id}`}>{item.title}</Link>
+              </ListGroupItem>
+            );
+          })}
+        </ListGroup>
       </div>
-      <ul>
-        {filmsByDirector.map((film) => (
-          <li key={film.id}>
-            <Link to={`film/${film.id}`}>
-              <h2>{film.title}</h2>
-            </Link>
-            <p>{film.release_date}</p>
-            <p>Critics: {film.rt_score}%</p>
-            <img src={film.image} alt={film.title + " banner"} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    </MainLayout>
   );
 }
